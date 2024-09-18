8bfd29f33b530f99e6d72907f7eb9acd31575549
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-18 13:59:46 +0300

    Add user filter

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from app import app
+from os import getenv
+from pyrogram.filters import user
 from pyrogram.client import Client
 from pyrogram.types import Message
 
-@app.on_message(filters=None)
+@app.on_message(filters=user(int(getenv(key='MY_ID'))))
 def on_message(_: Client, message: Message) -> None:
     print(message.text)
 
